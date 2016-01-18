2e7a9b48f5d6c8de48d171f614c0168b993cd4df
Sandesh Kalantre <kalantresandesh@gmail.com>
2016-01-18 10:45:14 +0530

    epsilon added to rhs, problem persists but in control

--- oscillator.py
+++ oscillator.py
@@ -4,26 +4,27 @@ from scipy.integrate import odeint
 
 #number of simulation points
 N = 100
-t = np.linspace(0,50,N)
+t = np.linspace(0,100,N)
 
 #damping
-gamma = 0.1
+gamma = 0.01
 #natural frequency
-w_0 = 1
+w_0 = 5
+eps = 1e-8
 
 def dy_dt(y,t):
-    return np.array([y[1],-1.0 * gamma*y[1] - w_0**2 * y[0]])
+    return np.array([y[1]+eps,eps-1.0 * gamma*y[1] - w_0**2 * y[0]])
 
 #initial conditions
 y0 = np.array([2,0])
 
-
 result = odeint(dy_dt,y0,t)
 
-plt.plot(t,result[:,0],label=r'$\gamma = $' + str(gamma) + ' $\omega_0 = $' + str(w_0))
-plt.xlabel('Time')
-plt.ylabel('x')
-plt.legend()
-plt.grid(True)
+plt.plot(t,result[:,0],label=r'$\gamma = $' + str(gamma) + ' $\omega_0 = $' + str(w_0) + '$\\ x_0 = $' + str(y0[0]) + ' $v_0 = $' + str(y0[1]),linewidth=3.0)
+plt.plot([0,10],[0,0],'r--',linewidth=3.0)
+plt.xlabel('Time',fontsize=24)
+plt.ylabel('x',fontsize=24)
+plt.legend(fontsize=24)
+plt.ylim([-2,2])
 plt.show()
 
